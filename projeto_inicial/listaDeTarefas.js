0c26ab66bf2f8bbce27058a1000aeb55f12f97a9
elias <elias99-souza@hotmail.com>
2022-04-05 00:41:14 -0300

    att

---
+++ projeto_inicial/listaDeTarefas.js
@@ -0,0 +1,9 @@
+const novaTarefa = document.querySelector('.form-button')
+
+
+novaTarefa.addEventListener('click', function(){
+    const input = document.querySelector(".form-input")
+    var valor = input.value
+    console.log(valor)
+
+})
